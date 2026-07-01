03ad1f9ff51182b71170465daa4eafd49445c36d
Omar Abdelfattah <jklk9579@gmail.com>
2026-07-01 03:54:07 +0300

    fix: use Netlify adapter and hybrid output for SSR pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omarcreatives.com',
-  output: 'static',
+  output: 'hybrid',
+  adapter: netlify(),
   integrations: [mdx(), sitemap(), preact()],
 });
